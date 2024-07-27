56ca546744c4dea8022850f0c86853ca0133f12f
Malathi <krishnavalluru99@gmail.com>
2024-07-27 12:06:04 +0530

    added 2nd script

--- 05-variables.sh
+++ 05-variables.sh
@@ -2,4 +2,7 @@
 echo "please enter your username"
 read -s USERNAME  # the vvalue entered above will be automatically attatched to username variable
 
-echo "Username is: $USERNAME"
+echo "please enter your password"
+read -s PASSWORD
+
+echo "Username is: $USERNAME, Password is: $PASSWORD" # I am printing just for validation, you should not print username and password in scripts

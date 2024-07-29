48ba67b8c6784b75c9d6686ec66851fc4312d81c
Malathi <krishnavalluru99@gmail.com>
2024-07-29 15:24:34 +0530

    added 2nd script

--- 11-logs.sh
+++ 11-logs.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
 ID=$(id -u)
-TIMESTAMP=$(date +%F-&H-&M-%S)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+R="\e[31m"
+G="\e[32m"
+O="\e[30m"
 
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "Script started executed at $TIMESTAMP" &>> $LOGFILE
 
 VALIDATE(){
 
 if [ $1 -ne 0 ]
 then 
-    echo "ERROR:: $2 ... failed"
+    echo -e "ERROR:: $2 ... $R failed $N"
     exit 1
 else
-    echo "$2 .... success" 
+    echo -e "$2 .... $G success $N" 
 fi
 }
 
 if [ $ID -ne 0 ]
 then 
-    echo "ERROR::Please run this script with root access"
+    echo -e "$R ERROR::Please run this script with root access $N"
     exit 1 #you can give other than 0
 else 
     echo "you are root user"

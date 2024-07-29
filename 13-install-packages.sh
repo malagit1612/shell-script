724f2f1ec06931ac95f528ba4f9fbaaa8e722231
Malathi <krishnavalluru99@gmail.com>
2024-07-29 22:34:30 +0530

    added 2nd script

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -38,7 +38,7 @@ do
     if [ $? -ne 0 ]  #if not installed
     then 
         yum install $package -y &>> $LOGFILE # install the package
-        VALIDATE $? "Installation of $packages" #validate
+        VALIDATE $? "Installation of $package" #validate
     else
         echo -e "$package is already installed ... $Y SKIPPING $N"
     fi    

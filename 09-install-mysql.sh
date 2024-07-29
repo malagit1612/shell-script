90d7cfc95b078ca7b33f2211681bfa46f47b8ed0
Malathi <krishnavalluru99@gmail.com>
2024-07-29 21:44:09 +0530

    added 2nd script

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -8,7 +8,7 @@ then
     exit 1 #you can give other than 0
 else 
     echo "you are root user"
-fi  # fi means reverse of id, indicating condition end
+fi  # fi means reverse of if, indicating condition end
 
 yum install mysql -y
 
